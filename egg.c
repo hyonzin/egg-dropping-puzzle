14658138ccadf31067705b5ac4326ccc95382975
hyonzin <hyeonjin507@gmail.com>
2016-11-30 20:11:05 +0900

    DP impl

--- egg.c
+++ egg.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
+#define MAX_EGG 1000000
+#define MAX_STORY 1000000
+
+int trial[MAX_EGG][MAX_STORY] = { 0, };
+
 int drop_egg (int egg, int story);
 int max (int n1, int n2);
 
 int main(int argc, char* argv[]) {
 
-	printf ("%d \n", drop_egg(3,2));
+	printf("%d \n", trial[0][1]);
+	printf ("%d \n", drop_egg(10,10001));
 
 	return 0;
 }
@@ -19,12 +25,22 @@ int drop_egg (int egg, int story) {
 		return story;
 
 	if (egg > 1) {
-		return 1 + max(
-				//case1. egg is broken at (story/2)
-				drop_egg(egg-1, (int)(story/2.0+0.5)-1),
-				//case2. egg is NOT broken at (story/2)
-				drop_egg(egg, (int)(story/2.0))
-				);
+		if (trial[egg][story] == 0) {
+
+			//case1. egg is broken at (story/2)
+			int doesBreak = drop_egg(egg-1, (int)(story/2.0)-1);
+
+			//case2. egg is NOT broken at (story/2)
+			int doesNotBreak = drop_egg(egg, (int)(story/2.0));
+
+			trial[egg][story] = 1 + max(doesBreak, doesNotBreak);
+			printf("update trial[%d][%d] = %d \n", egg, story, trial[egg][story]);
+	
+		} else {
+			printf("use existed trial[%d][%d] = %d \n", egg, story, trial[egg][story]);
+		}
+
+		return trial[egg][story];
 	}
 
 	return 0;
