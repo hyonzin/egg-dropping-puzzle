ed9a03ddb01d1428f0eb6a3a9e25a172c5942347
hyonzin <hyeonjin507@gmail.com>
2016-11-30 20:58:39 +0900

    Show test progress

--- egg.c
+++ egg.c
@@ -3,44 +3,59 @@
 #define MAX_EGG 1000000
 #define MAX_STORY 1000000
 
-int trial[MAX_EGG][MAX_STORY] = { 0, };
+int test[MAX_EGG][MAX_STORY] = { 0, };
 
-int drop_egg (int egg, int story);
+int drop_egg_main (int egg, int story);
+int drop_egg (int egg, int story, int depth);
 int max (int n1, int n2);
 
 int main(int argc, char* argv[]) {
 
-	printf("%d \n", trial[0][1]);
-	printf ("%d \n", drop_egg(10,10001));
+	if (argc < 3) {
+		printf("%s {egg} {story}\n", argv[0]);
+		return -1;
+	}
 
+	int egg = atoi(argv[1]);
+	int story = atoi(argv[2]);
+
+	printf ("        ... drop_egg_main (%d, %d) = %d \n", egg, story, drop_egg_main(egg, story));
 	return 0;
 }
 
-int drop_egg (int egg, int story) {
+int drop_egg_main (int egg, int story) {
+	return drop_egg(egg, story, 0);
+}
 
-	if (story < 1)
-		return 0;
+int drop_egg (int egg, int story, int depth) {
 
-	if (egg == 1)
+	if (story == 1 || story == 0) {
 		return story;
+	}
+
+	if (egg == 1) {
+		printf("      - Only one egg remains. Return story %d \n", story);
+		return story;
+	}
 
 	if (egg > 1) {
-		if (trial[egg][story] == 0) {
+		if (test[egg][story] == 0) {
+			printf("(%d) Trying %d-egg, %d-story -> %d \n", depth, egg, story, (int)(story/2.0));
 
 			//case1. egg is broken at (story/2)
-			int doesBreak = drop_egg(egg-1, (int)(story/2.0)-1);
+			int doesBreak = drop_egg(egg-1, (int)(story/2.0)-1, depth+1);
 
 			//case2. egg is NOT broken at (story/2)
-			int doesNotBreak = drop_egg(egg, (int)(story/2.0));
+			int doesNotBreak = drop_egg(egg, (int)(story/2.0), depth+1);
 
-			trial[egg][story] = 1 + max(doesBreak, doesNotBreak);
-			printf("update trial[%d][%d] = %d \n", egg, story, trial[egg][story]);
+			test[egg][story] = 1 + max(doesBreak, doesNotBreak);
+			printf("    > update test[%d][%d] = %d \n", egg, story, test[egg][story]);
 	
 		} else {
-			printf("use existed trial[%d][%d] = %d \n", egg, story, trial[egg][story]);
+			printf("    > reuse test[%d][%d] = %d \n", egg, story, test[egg][story]);
 		}
 
-		return trial[egg][story];
+		return test[egg][story];
 	}
 
 	return 0;
